d0bd8ff7e40053ddd08aa5d4f13888d22e956897
Yi Lu <zhiwudazhanjiangshi@gmail.com>
2023-01-23 21:44:00 +0800

    Create nettango-update.sh

---
+++ nettango-update.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+git pull https://github.com/NetLogo/NetTango
+git pull https://github.com/NetLogo/Galapagos
+
+cd ./NetTango
+git submodule update --remote --recursive
+
+npm link
+npm link @netlogo/nettango
+npm audit fix
+npm run build
+
+#cp nettango.js ../Galapagos/target/web/public/main/nettango/nettango.js
+#cp nettango.css ../Galapagos/target/web/public/main/nettango/nettango.css
+cp -R ./nt-models/. ../Galapagos/public/nt-modelslib
+
+# run
+cd ../Galapagos
+sbt run
