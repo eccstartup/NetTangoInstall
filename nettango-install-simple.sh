aa927c3be7fb4e9f8f5255ae5975b12a611838c1
Yi Lu <zhiwudazhanjiangshi@gmail.com>
2023-02-10 20:46:32 +0800

    Update nettango-install-simple.sh

--- nettango-install-simple.sh
+++ nettango-install-simple.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-git clone https://github.com/NetLogo/NetTango
 git clone https://github.com/NetLogo/Galapagos
 
-cd ./NetTango
+cd Galapagos
 git submodule update --init --recursive
-cp -R ./nt-models/. ../Galapagos/public/nt-modelslib
 
-cd ../Galapagos
+npm audit fix --force  # optional
+
 sbt run
